e46f333f592b1f7fd341c66177b1669c9bf2a591
soumitra <soumitra.petbindhi@gmail.com>
2023-08-31 20:19:43 +0530

    add onboarding page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
 import { OnBoarding } from '..'
-
+import "../dist/style.css";
 
 function App() {
-  return (
-    <div>
-      <OnBoarding />
-    </div>
-  )
+    const answers = (ans) => {
+        console.log(ans);
+    };
+    return (
+        <div>
+            <OnBoarding
+                entityId="e-0000000000"
+                questId="q-296268eb-5731-43b2-8de2-8077f809f139"
+                userId="u-4cc510a4-e670-4ed3-abe3-c351c19bb992"
+                design={[
+                    [1, 4],
+                    [2, 3],
+                ]}
+                answers={answers}
+                multiChoice="modal2"
+                headingScreen={[
+                    { name: "n1", desc: "sdes" },
+                    { name: "dd", desc: "es" },
+                ]}
+                color="red"
+                btnColor="blue"
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
