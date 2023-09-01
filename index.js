2ff8740191db6e03d64278b17c2e2ad2e2308c29
Prajwal Raut <rautprajwal546@gmail.com>
2023-09-01 10:14:59 +0530

    removed older files

--- index.js
+++
@@ -1,6 +0,0 @@
-import './src/bootstrap';
-import './index.css';
-import './style.css';
-
-export { default as QuestLogin } from './src/components/Login';
-export { default as QuestPricing } from './src/components/QuestPricing';
