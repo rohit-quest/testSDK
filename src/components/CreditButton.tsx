28485cef000650bcfde1f00175eaa600e975e212
soumitra-dey <soumitra.petbindhi@gmail.com>
2023-09-05 18:26:31 +0530

    update css 2

--- src/components/CreditButton.tsx
+++ src/components/CreditButton.tsx
@@ -11,13 +11,13 @@ interface CreditButtonProps {
 
 
 
-const CreditButton: FC<CreditButtonProps> = ({remainingCount, fontSize, addButton, btnText, buttonFunction}) => {
+const CreditButton: FC<CreditButtonProps> = ({ remainingCount, fontSize, addButton, btnText, buttonFunction }) => {
 
 
     return (
         <div className='questLabs'>
             <div className='flex bg-white rounded-xl shadow-lg relative py-6 px-14 w-fit gap-12 items-center'>
-                <p className="text-lg font-bold text-gray-700" style={{fontSize:`${fontSize}`}}>Remaining credits : <span className='text-black'>{remainingCount}</span></p>
+                <p className="text-lg font-bold text-gray-700" style={{ fontSize: `${fontSize}` }}>Remaining credits : <span className='text-black'>{remainingCount}</span></p>
                 {
                     !!addButton &&
                     <button onClick={() => buttonFunction()} className='text-white bg-gray-700 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-base py-3 px-5'>{btnText}</button>
