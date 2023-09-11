20fba8d792662bb5de0e72b33b2cdea4c38e151a
1Gireesh <gireeshbhat68@gmail.com>
2023-09-11 09:57:57 +0530

    1 to 1 quest form

--- src/components/QuestForm/index.tsx
+++ src/components/QuestForm/index.tsx
@@ -1,12 +1,16 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import QuestContext from '../../components/QuestWrapper';
-import { ApiResponse, fetchQuestions, metadata, questFormPropType } from "./response.ts";
+import {ApiResponse, fetchQuestions, metadata, questFormPropType} from "./response.ts";
 import enterPng from '../../assets/images/enter.png';
 
 
 export const QuestForm = (props: questFormPropType) => {
 
-    const { userId = "", questId = "", token = "", setAnswer = (() => { }), onSubmit = (() => { }) } = props;
+    const {
+        userId = "", questId = "", token = "", setAnswer = (() => {
+        }), onSubmit = (() => {
+        })
+    } = props;
     const headers = useContext(QuestContext.Context) || null;
 
     const [criteria, setCriteria] = useState<metadata[]>([]);
@@ -16,52 +20,64 @@ export const QuestForm = (props: questFormPropType) => {
 
     let current = Date.now();
 
-    const { color = "", bgColor = "", progressBar,
+    const {
+        color = "", bgColor = "", progressBar,
         alignment = "start", inputBorderColor = "transparent",
         headingSize = "24px", descSize = "24px"
     } = props;
     const setData = async () => {
-        const res = await fetchQuestions({ ...headers, userId, questId, token });
+        const res = await fetchQuestions({...headers, userId, questId, token});
         if (!res?.success) return;
         setSubject(res);
         let crts = res?.eligibilityData.map(e => ({
             ...e.data.metadata, ...e.data
         }))
         setCriteria(crts);
-        setAnswer(crts.map(({ title, options }) => ({ question: title, answer: options || "" })))
+        setAnswer(crts.map(({title, options}) => ({question: title, answer: options || ""})))
     }
     const [anime, setAnime] = useState<"scroll-animation" | "scroll-animation-rev" | "">("")
 
     // @ts-ignore
-    const TextArea = ({ setFill, title = "" }: { setFill: React.Dispatch<React.SetStateAction<string>>, title: string }) => {
+    const TextArea = ({setFill, title = ""}: {
+        setFill: React.Dispatch<React.SetStateAction<string>>,
+        title: string
+    }) => {
         return (<textarea cols={60} placeholder={"enter the " + title} className='!p-[10px]'
-            onChange={(e) => {
-                setFill(e.target.value)
-                setAnswer(prev => {
-                    prev[page].answer = e.target.value;
-                    return prev;
-                })
-            }}
-            style={{ color: color, border: `1px solid ${inputBorderColor}`, backgroundColor: bgColor }}
-            rows={5}></textarea>)
+                          onChange={(e) => {
+                              setFill(e.target.value)
+                              setAnswer(prev => {
+                                  prev[page].answer = e.target.value;
+                                  return prev;
+                              })
+                          }}
+                          style={{color: color, border: `1px solid ${inputBorderColor}`, backgroundColor: bgColor}}
+                          rows={5}></textarea>)
     }
 
     // @ts-ignore
-    const Radio = ({ setFill, options = ["option1", "option2", "option3"] }: { setFill: React.Dispatch<React.SetStateAction<string>>, options: Array<string> }) => {
+    const Radio = ({setFill, options = ["option1", "option2", "option3"]}: {
+        setFill: React.Dispatch<React.SetStateAction<string>>,
+        options: Array<string>
+    }) => {
 
         return (<div className="flex gap-x-12 gap-y-2 flex-wrap">
             {options.map((option: string, id: number) => (
                 <div className="flex items-center mb-3" key={id}>
                     <input id={`sct${id}`} type="radio" value={option}
-                        onChange={(e) => {
-                            setFill(e.target.value)
-                            setAnswer(prev => {
-                                prev[page].answer = e.target.value;
-                                return prev;
-                            })
-                        }}
-                        name="default-radio"
-                        className="h-4 w-4 accent-black  bg-grey-700 text-red-500 rounded cursor-pointer"
+                           defaultChecked={(!!values[page]?.length) && values[page] == option}
+                           onChange={(e) => {
+                               setFill(e.target.value)
+                               setValues(prev => {
+                                   prev[page] = e.target.value;
+                                   return prev;
+                               })
+                               setAnswer(prev => {
+                                   prev[page].answer = e.target.value;
+                                   return prev;
+                               })
+                           }}
+                           name="default-radio"
+                           className="h-4 w-4 accent-black  bg-grey-700 text-red-500 rounded cursor-pointer"
                     />
                     <label
                         htmlFor={`sct${id}`}
@@ -74,11 +90,22 @@ export const QuestForm = (props: questFormPropType) => {
         </div>)
     }
 
-    const Multi = ({ setFill, options = ["option1", "option2", "option3"] }: { setFill: React.Dispatch<React.SetStateAction<string>>, options: Array<string> }) => {
+    const Multi = ({setFill, options = ["option1", "option2", "option3"]}: {
+        setFill: React.Dispatch<React.SetStateAction<string>>,
+        options: Array<string>
+    }) => {
+        let selected: Array<string> = [];
+        useEffect(() => {
+            if (Array.isArray(values[page])) {
+                selected = values[page] as Array<string>;
+                setFill(selected[0]);
+            }
+        }, []);
+
         return (<div className="py-3">
             <p
                 className="block mb-1 font-medium"
-                style={{ color: color }}
+                style={{color: color}}
             >
             </p>
             <ul className="flex flex-wrap w-full gap-3">
@@ -88,13 +115,41 @@ export const QuestForm = (props: questFormPropType) => {
                             type="checkbox"
                             id={`mct${id}`}
                             value={option}
+                            defaultChecked={(!!values[page]?.length) && values[page].includes(option)}
                             className="hidden peer"
                             onChange={(e) => {
                                 setFill(e.target.value)
+                                setValues(prev => {
+                                    if (Array.isArray(prev[page])) {
+                                        if (!e.target.checked)
+                                            for (let i = prev[page].length - 1; i >= 0; i--) {
+                                                if (prev[page][i] === e.target.value) {
+                                                    // @ts-ignore
+                                                    prev[page].splice(i, 1);
+                                                }
+                                            }
+                                        else {
+                                            // @ts-ignore
+                                            prev[page].push(e.target.value);
+                                        }
+                                    } else {
+                                        prev[page] = [e.target.value];
+                                    }
+                                    return prev;
+                                })
                                 setAnswer(prev => {
                                     if (Array.isArray(prev[page].answer)) {
-                                        // @ts-ignore
-                                        prev[page].answer.push(e.target.value);
+                                        if (!e.target.checked)
+                                            for (let i = prev[page].answer.length - 1; i >= 0; i--) {
+                                                if (prev[page]?.answer[i] === e.target.value) {
+                                                    // @ts-ignore
+                                                    prev[page].answer.splice(i, 1);
+                                                }
+                                            }
+                                        else {
+                                            // @ts-ignore
+                                            prev[page].answer.push(e.target.value);
+                                        }
                                     } else {
                                         prev[page].answer = [e.target.value];
                                     }
@@ -116,44 +171,65 @@ export const QuestForm = (props: questFormPropType) => {
         </div>)
     }
 
-    const NormalInput = ({ title = "", setFill }: { title: string, setFill: React.Dispatch<React.SetStateAction<string>> }) => {
+    const NormalInput = ({title = "", setFill}: {
+        title: string,
+        setFill: React.Dispatch<React.SetStateAction<string>>
+    }) => {
         const input = useRef<HTMLInputElement>(null);
         useEffect(() => {
-            if (!!input.current) input.current.focus();
+            if (!!input.current) {
+                if (typeof values[page] === "string") {
+                    input.current.value = values[page] as string;
+                    setFill(values[page] as string)
+                }
+                input.current.focus();
+            }
         }, []);
         return (<input placeholder={"Enter Your " + title}
-            style={{ color: color, border: `1px solid ${inputBorderColor}`, backgroundColor: bgColor }}
-            ref={input}
-            // value={values[page]}
-            onChange={(e) => {
-                setFill(e.target.value)
-                // setValues(prev => {
-                //     prev[page] = e.target.value;
-                //     return [...prev]
-                // })
-                setAnswer(prev => {
-                    prev[page].answer = e.target.value;
-                    return prev;
-                })
-            }}
-            className='h-[35px] text-black py-5 focus:border-none focus:outline-none' />)
+                       style={{color: color, border: `1px solid ${inputBorderColor}`, backgroundColor: bgColor}}
+                       ref={input}
+                       onChange={(e) => {
+                           setFill(e.target.value)
+                           setValues(prev => {
+                               prev[page] = e.target.value;
+                               return prev
+                           })
+                           setAnswer(prev => {
+                               prev[page].answer = e.target.value;
+                               return prev;
+                           })
+                       }}
+                       className='h-[35px] text-black py-5 focus:border-none focus:outline-none'/>)
     }
 
-    const DateInput = ({ setFill }: { setFill: React.Dispatch<React.SetStateAction<string>> }) => {
+    const DateInput = ({setFill}: { setFill: React.Dispatch<React.SetStateAction<string>> }) => {
+        const input = useRef<HTMLInputElement>(null);
+        useEffect(() => {
+            if (!!input.current) {
+                if (typeof values[page] === "string") {
+                    input.current.value = values[page] as string;
+                    setFill(values[page] as string)
+                }
+            }
+        }, []);
         return (
-            <div className="py-3" >
-                <label className="block mb-1 font-medium" htmlFor="dateInput" style={{ color: color }}>
+            <div className="py-3">
+                <label className="block mb-1 font-medium" htmlFor="dateInput" style={{color: color}}>
                 </label>
-                <input type="date" id="dateInput" name="dateInput"
-                    style={{ color: color, border: `1px solid ${inputBorderColor}`, backgroundColor: bgColor }}
-                    className="bg-gray-100 border-none outline-none text-sm rounded focus:ring-blue-500 focus:ring-1 w-full p-3"
-                    onChange={(e) => {
-                        setFill(e.target.value)
-                        setAnswer(prev => {
-                            prev[page].answer = e.target.value;
-                            return prev;
-                        })
-                    }}
+                <input type="date" id="dateInput" name="dateInput" ref={input}
+                       style={{color: color, border: `1px solid ${inputBorderColor}`, backgroundColor: bgColor}}
+                       className="bg-gray-100 border-none outline-none text-sm rounded focus:ring-blue-500 focus:ring-1 w-full p-3"
+                       onChange={(e) => {
+                           setFill(e.target.value)
+                           setValues(prev => {
+                               prev[page] = e.target.value;
+                               return prev
+                           })
+                           setAnswer(prev => {
+                               prev[page].answer = e.target.value;
+                               return prev;
+                           })
+                       }}
                 />
             </div>
         );
@@ -167,12 +243,13 @@ export const QuestForm = (props: questFormPropType) => {
 
         window.onkeydown = (event: KeyboardEvent) => {
             if (event.key === "Enter") {
-                if (page == criteria.length - 1)
-                    onSubmit();
                 if (subj.isOptional || fillVal) {
                     setAnime("scroll-animation")
                     setPage(c => c + 1)
                     setFill("");
+                    if (page == criteria.length - 1) {
+                        onSubmit();
+                    }
                 } else {
                     setNext(false)
                 }
@@ -190,8 +267,7 @@ export const QuestForm = (props: questFormPropType) => {
                 } else {
                     setNext(false)
                 }
-            }
-            else if (e.deltaY < 0 && page > -1) {
+            } else if (e.deltaY < 0 && page > -1) {
                 setAnime("scroll-animation-rev")
                 setPage(c => c - 1)
             }
@@ -203,16 +279,22 @@ export const QuestForm = (props: questFormPropType) => {
                     <h4 className={`font-normal text-[${descSize}]`}>{subj?.title}</h4>
                     <div>
                         {(() => {
-                            if (page == 1)
-                                return <NormalInput setFill={setFill} title={subj?.title} />
-                            if (page == 2)
-                                return <Multi options={subj.options} setFill={setFill} />
-                            if (page == 3)
-                                return <Radio options={subj.options} setFill={setFill} />
-                            if (page == 4)
-                                return <DateInput setFill={setFill} />
-                            else
-                                return <NormalInput title={subj?.title || ""} setFill={setFill} />
+                            switch (subj.criteriaType) {
+                                case "":
+                                    return <NormalInput setFill={setFill} title={subj?.title}/>
+                                case "USER_INPUT_TEXT":
+                                    return <NormalInput setFill={setFill} title={subj?.title}/>
+                                case "USER_INPUT_MULTI_CHOICE":
+                                    return <Multi options={subj.options} setFill={setFill}/>
+                                case "USER_INPUT_SINGLE_CHOICE":
+                                    return <Radio options={subj.options} setFill={setFill}/>
+                                case "USER_INPUT_DATE":
+                                    return <DateInput setFill={setFill}/>
+                                case "USER_INPUT_TEXT_AREA":
+                                    return <TextArea setFill={setFill} title={subj.title}/>
+                                default :
+                                    return <NormalInput setFill={setFill} title={subj?.title}/>
+                            }
                         })()}
                         <div className='text-red-600'>{!allowNext && `please fill the ${subj.title}`}</div>
                     </div>
@@ -239,7 +321,7 @@ export const QuestForm = (props: questFormPropType) => {
                         </button>}
                         <div className='flex whitespace-nowrap gap-5 font-[300] items-center'>
                             Press Enter
-                            <img className='w-8' src={enterPng} alt='' /></div>
+                            <img className='w-8' src={enterPng} alt=''/></div>
                     </div>
                 </div>)}
         </>)
@@ -271,7 +353,7 @@ export const QuestForm = (props: questFormPropType) => {
                         className={`w-[165px] text-[24px] h-14 px-11 py-6 !text-white !bg-black rounded-lg border justify-center items-center gap-2 inline-flex`}>Next
                     </button>
                     <div className='flex whitespace-nowrap gap-5 font-[300] items-center'>Press Enter
-                        <img className='w-8' src={enterPng} alt='' /></div>
+                        <img className='w-8' src={enterPng} alt=''/></div>
                 </div>
             </div>)}
         </>)
@@ -284,13 +366,13 @@ export const QuestForm = (props: questFormPropType) => {
     return (
         <div className='questLabs flex flex-col h-[100vh]'>
             {progressBar &&
-                <div style={{ width: `${String((page + 1) / (criteria.length - 1) * 100)}vw` }}
-                    className={`h-2 bg-blue-700 fixed`}></div>}
+                <div style={{width: `${String((page + 1) / (criteria.length - 1) * 100)}vw`}}
+                     className={`h-2 bg-blue-700 fixed`}></div>}
             <div style={{
                 backgroundColor: bgColor,
                 color: color,
             }} className={`relative !w-[100%] h-[95vh] flex justify-${alignment} items-center`}>
-                {(page < 0) ? <FirstPage /> : <Survey />}
+                {(page < 0) ? <FirstPage/> : <Survey/>}
             </div>
         </div>
     );
--- src/components/QuestForm/response.ts
+++ src/components/QuestForm/response.ts
@@ -1,107 +1,108 @@
-
-
 import React from "react";
 import config from "../../config";
-import axios, { AxiosError } from "axios";
+import axios, {AxiosError} from "axios";
+
 export interface questFormPropType {
-  alignment?: "center" | "start" | "end";
-  color?: string;
-  bgColor?: string;
-  progressBar?: boolean;
-  inputBorderColor?: string;
-  screenHeight?: string;
-  answer?: any;
-  setAnswer: React.Dispatch<React.SetStateAction<Array<{ question: string, answer: string | Array<string> }>>>;
-  inputBorder?: string;
-  headingSize?: string;
-  descSize?: string;
-  inputFieldType?: object;
-  userId?: string;
-  token?: string;
-  questId?: string;
-  onSubmit?: () => void
+    alignment?: "center" | "start" | "end";
+    color?: string;
+    bgColor?: string;
+    progressBar?: boolean;
+    inputBorderColor?: string;
+    screenHeight?: string;
+    answer?: any;
+    setAnswer: React.Dispatch<React.SetStateAction<Array<{ question: string, answer: string | Array<string> }>>>;
+    inputBorder?: string;
+    headingSize?: string;
+    descSize?: string;
+    inputFieldType?: object;
+    userId?: string;
+    token?: string;
+    questId?: string;
+    onSubmit?: () => void
 }
+
 export interface metadata {
-  title: string;
-  effort: string;
-  importance: string;
-  xp: number;
-  frequency: string;
-  isOptional: boolean;
-  placeholder: string;
-  options: Array<string>;
+    title: string;
+    effort: string;
+    importance: string;
+    xp: number;
+    frequency: string;
+    isOptional: boolean;
+    placeholder: string;
+    options: Array<string>;
+    criteriaType? :"USER_INPUT_TEXT" | "USER_INPUT_DATE" |"USER_INPUT_SINGLE_CHOICE" | "USER_INPUT_MULTI_CHOICE" | "USER_INPUT_TEXT_AREA" |"";
 }
 
 export interface ApiResponse {
-  data: {
-    eligibilityCriterias: string[];
-    rewards: [];
-    isDeleted: boolean;
-    hasReferral: boolean;
-    referralXP: number;
-    visibility: string;
-    status: string;
-    xp: number;
-    minXPThreshold: number;
-    createdAt: string;
-    isPrivate: boolean;
-    questId: string;
-    entityId: string;
-    title: string;
-    description: string;
-    imageURL: string;
-    endsAt: string;
-    theme: {
-      accentColor: string;
-      backgroundColor: string;
-      fontFamily: string;
-      layout: string;
+    data: {
+        eligibilityCriterias: string[];
+        rewards: [];
+        isDeleted: boolean;
+        hasReferral: boolean;
+        referralXP: number;
+        visibility: string;
+        status: string;
+        xp: number;
+        minXPThreshold: number;
+        createdAt: string;
+        isPrivate: boolean;
+        questId: string;
+        entityId: string;
+        title: string;
+        description: string;
+        imageURL: string;
+        endsAt: string;
+        theme: {
+            accentColor: string;
+            backgroundColor: string;
+            fontFamily: string;
+            layout: string;
+        };
+        category: string;
+        skills: [];
     };
-    category: string;
-    skills: [];
-  };
-  eligibilityData: EligibilityData[];
-  allCriteriasCompleted: boolean;
-  success: boolean;
-  claimStatus: boolean;
+    eligibilityData: EligibilityData[];
+    allCriteriasCompleted: boolean;
+    success: boolean;
+    claimStatus: boolean;
 }
 
 interface EligibilityData {
-  data: {
-    xp: number;
-    dependentCriterias: string[];
-    frequency: string;
-    criteriaType: string;
-    createdAt: string;
-    criteriaId: string;
-    questId: string;
-    metadata: metadata;
-    requiresApproval: boolean;
-  };
-  completed: boolean;
-  isLocked: boolean;
-  unfinishedCriteriaIds: string[];
+    data: {
+        xp: number;
+        dependentCriterias: string[];
+        frequency: string;
+        createdAt: string;
+        criteriaId: string;
+        questId: string;
+        criteriaType :"USER_INPUT_TEXT" | "USER_INPUT_DATE" |"USER_INPUT_SINGLE_CHOICE" | "USER_INPUT_MULTI_CHOICE" | "USER_INPUT_TEXT_AREA" |"";
+        metadata: metadata;
+        requiresApproval: boolean;
+    };
+    completed: boolean;
+    isLocked: boolean;
+    unfinishedCriteriaIds: string[];
 }
 
 export async function fetchQuestions({
-  apiKey = "",
-  apiSecret = "",
-  userId = "",
-  token = "",
-  entityId = "e-cbd250cc-3fcb-4085-a95e-712742ffa7ac",
-  questId = ""
-}) {
-  const headers = { apiKey, apiSecret, userId, token };
-  const request = `${config.BACKEND_URL}api/entities/${entityId}/quests/${questId}?userId=${userId}`;
+                                         apiKey = "",
+                                         apiSecret = "",
+                                         userId = "",
+                                         token = "",
+                                         entityId = "e-cbd250cc-3fcb-4085-a95e-712742ffa7ac",
+                                         questId = ""
+                                     }) {
+    const headers = {apiKey, apiSecret, userId, token};
+    const request = `${config.BACKEND_URL}api/entities/${entityId}/quests/${questId}?userId=${userId}`;
 
-  try {
-    const res = await axios.get(request, { headers });
-    let response: ApiResponse = res.data;
-    return response as ApiResponse;
-  } catch (error) {
-    if (error instanceof AxiosError)
-      console.log(error.message)
-    else console.log(error);
-    return null;
-  }
+    try {
+        const res = await axios.get(request, {headers});
+        let response: ApiResponse = res.data;
+        return response as ApiResponse;
+    } catch (error) {
+        if (error instanceof AxiosError)
+            console.log(error.message)
+        else console.log(error);
+        return null;
+    }
 }
