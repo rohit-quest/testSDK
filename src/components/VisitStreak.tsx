69f3a4dfd15471db1a2c59faa3043765c8666a9c
1Gireesh <gireeshbhat68@gmail.com>
2023-09-14 22:28:10 +0530

    converted tailwind to css

--- src/components/VisitStreak.tsx
+++ src/components/VisitStreak.tsx
@@ -1,4 +1,4 @@
-import './styles/VisitStreak.css';
+import './VisitStreak.css';
 import streakBg from '../assets/images/streakHexa.png';
 import streak from '../assets/images/streak.png';
 
@@ -6,19 +6,18 @@ import streak from '../assets/images/streak.png';
 export const VisitStreak = () => {
     return (
         <div className='m-5'>
-            <div className='visit-streak flex gap-[55px] items-center rounded-[10px] shadow-[0px_0px_7px_#00000059] px-[16px] py-[20px] w-fit'>
-                <div className='flex flex-col gap-2'>
-                    <div className='text-black text-xs font-medium leading-[18px]'>You have earned a new badge:
-                        <span className='font-bold'>Daily Streaking</span></div>
-                    <div className='text-black text-xs font-medium leading-[18px]'>You maintained a streak for {25} days</div>
+            <div className='q-streak-main'>
+                <div className='q-streak-div'>
+                    <div className='q-streak-badge'>You have earned a new badge:
+                        <span style={{fontWeight: 700}}>Daily Streaking</span></div>
+                    <div className='q-streak-text'>You maintained a streak
+                        for {25} days
+                    </div>
                 </div>
-                <div className='relative'>
-                    <img src={streakBg} alt="" style={{
-                        WebkitFilter: 'drop-shadow(2px 2px 5px rgba(34, 34, 34, 0.65))',
-                        filter: 'drop-shadow(2px 2px 5px rgba(34, 34, 34, 0.65))',
-                    }} className='h-[58px]' />
-                    <img src={streak} alt='' className='absolute top-[25%] w-[12.47px] h-[12.47px] left-[38%]' />
-                    <span className='absolute text-white top-[65%] left-[38%] text-[12px]'>{25}</span>
+                <div style={{position: "relative"}}>
+                    <img src={streakBg} alt=""  className='q-streak-img'/>
+                    <img src={streak} alt='' className='q-streak-simg'/>
+                    <span className='q-streak-count'>{25}</span>
                 </div>
             </div>
         </div>
