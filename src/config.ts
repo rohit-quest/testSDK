4545e676e4cc267daeff02ec440259f2bb4ee848
soumitra-dey <soumitra.petbindhi@gmail.com>
2023-09-27 13:13:16 +0530

    add onboardings

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 class config {
-    static BACKEND_URL = 'https://api.questprotocol.xyz/';
+    static BACKEND_URL = 'http://localhost:8081/';
     static FLAG_CONSTRAINTS = {
         OnboardingFlag: "quest.sdk.onboardings",
         AnalyticsFlag: "quest.sdk.analytics",
