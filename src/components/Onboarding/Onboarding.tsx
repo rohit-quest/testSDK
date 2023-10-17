cc8b4dbdfeec8a74a6d1b8af874628be44d90297
soumitra-dey <soumitra.petbindhi@gmail.com>
2023-10-17 13:18:29 +0530

    fix progressbar

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -50,6 +50,8 @@ interface QuestLoginProps {
     previousBtnText: string;
     nextBtnText: string;
     progressbarColor: string;
+    progressBarMultiLine: boolean;
+    progressBartabHeight: string;
 }
 
 interface FormData {
@@ -101,6 +103,8 @@ function OnBoarding(props: QuestLoginProps) {
         previousBtnText,
         nextBtnText,
         progressbarColor,
+        progressBarMultiLine,
+        progressBartabHeight
     } = props;
 
     const [formdata, setFormdata] = useState<FormData[] | []>([]);
@@ -304,13 +308,20 @@ function OnBoarding(props: QuestLoginProps) {
                                         </div>
                                     </div>
                                     :
-                                    <div className="q-onb-progress-comp" style={{borderColor: "#EAEBED"}}>
+                                    <div className="q-onb-progress-comp" style={{borderColor: "#EAEBED", height: progressBarMultiLine ? progressBartabHeight : "auto"}}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                             <path d="M8 0.25C3.71978 0.25 0.25 3.71978 0.25 8C0.25 12.2802 3.71978 15.75 8 15.75C12.2802 15.75 15.75 12.2802 15.75 8C15.75 3.71978 12.2802 0.25 8 0.25ZM10.5 8C10.5 9.3785 9.3785 10.5 8 10.5C6.6215 10.5 5.5 9.3785 5.5 8C5.5 6.6215 6.6215 5.5 8 5.5C9.3785 5.5 10.5 6.6215 10.5 8Z" 
                                                 fill="#EAEBED"
                                             />
                                         </svg>
-                                        <div style={{width: `${((((wd - (progress.length - 1) * 15)) / progress.length) - 24)}px`}}>
+                                        <div 
+                                            style={{
+                                                    width: `${((((wd - (progress.length - 1) * 15)) / progress.length) - 24)}px`, 
+                                                    whiteSpace: progressBarMultiLine ? "normal" : "nowrap", 
+                                                    overflow: progressBarMultiLine ? "" : "hidden", 
+                                                    textOverflow: progressBarMultiLine ? "" : "ellipsis",
+                                                }}
+                                            >
                                             {prog}
                                         </div>
                                     </div>
