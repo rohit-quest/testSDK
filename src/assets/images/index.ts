26264ca3ca57abd7e7243ab2b8368b533f191a4e
DhaanuI <dhaanu97@gmail.com>
2023-11-03 22:48:28 +0530

    referEarn polish

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -14,8 +14,13 @@ import telegramPng from './telegram.png';
 import giftPng from './referEarn.png';
 import creditBannerPng from './creditBanner.png'
 import creditCheck from './creditCheck.png'
+import whatsappSvg from './whatsapp.svg';
+import twitterSvg from './twitter.svg';
+import discordSvg from './discord.svg';
+import copySVG from './copy.svg';
 
-export { googleImg, arrow, googleSvg, whiteStar, blackStar, tick, disLike, like,closePng,copyPng,twitterPng,telegramPng,discordPng,giftPng,creditBannerPng,creditCheck };
+
+export { copySVG, googleImg, arrow, googleSvg, whiteStar, blackStar, tick, disLike, like,closePng,copyPng,twitterPng,telegramPng,discordPng,giftPng,creditBannerPng,creditCheck, whatsappSvg, twitterSvg, discordSvg};
 
 
 
--- src/components/Refer/ReferEarn.tsx
+++ src/components/Refer/ReferEarn.tsx
@@ -1,60 +1,110 @@
-import {closePng, copyPng, giftPng, telegramPng, twitterPng, tick} from "../../assets/images";
-import './Refer.css';
-import {useContext, useEffect, useState} from "react";
-import {referProp, response, shareOnPlatform} from "./Response.ts";
+import {
+  closePng,
+  copySVG,
+  giftPng,
+  telegramPng,
+  twitterSvg,
+  tick,
+  whatsappSvg,
+  discordSvg,
+} from "../../assets/images";
+import "./Refer.css";
+import { useContext, useEffect, useState } from "react";
+import { referProp, response, shareOnPlatform } from "./Response.ts";
 import QuestContext from "../QuestWrapper.tsx";
 import PopupComponent from "./Popup.tsx";
 
-
 export const ReferShare = ({
-                               isOpen = true,
-                               onClose,
-                               questId = "",
-                               userId = "",
-                               token = '',
-                               color,
-                               bgColor
-                           }: referProp) => {
+  isOpen = true,
+  onClose,
+  questId = "",
+  userId = "",
+  token = "",
+  color,
+  bgColor,
+}: referProp) => {
+  const [shareCode, setCode] = useState("");
+  const [copy, setCopy] = useState(false);
+  const { apiKey, apiSecret, entityId } = useContext(QuestContext.Context);
+  const style = !!color && !!bgColor ? { color, backgroundColor: bgColor } : {};
 
-    const [shareCode, setCode] = useState("");
-    const [copy, setCopy] = useState(false);
-    const {apiKey, apiSecret, entityId} = useContext(QuestContext.Context);
-    const style =( !!color&&!! bgColor)?{color, backgroundColor: bgColor}:{};
+  useEffect(() => {
+    response(questId, {
+      apiKey,
+      userid: userId,
+      entityId,
+      apisecret: apiSecret,
+      token,
+    }).then((r) => setCode(r.referralCode || ""));
+  }, []);
 
-    useEffect(() => {
-        response(questId, {apiKey, userid: userId, entityId, apisecret: apiSecret, token})
-            .then(r => setCode(r.referralCode || ""));
-    }, []);
+  if (!isOpen) return <></>;
+  return (
+    <div className="q-referShare" style={style}>
+      <div className="q-referShare-content">
+        <div className="q-referShare-content-text">
+          <h3 className="q-referShare-content-head" style={style}>
+            Invite your friends
+          </h3>
+          <p className="q-referShare-content-para" style={style}>
+            Share your unique referral code with friends and receive 10 coins in
+            credits each time a friend signs up!
+          </p>
+          <div className="q-referShare-referralCode">
+            <p>Referral code</p>
+          </div>
+          <div className="q-referShare-content-rect" style={style}>
+            <span className="q-referShare-content-code" style={style}>
+              {shareCode}
+            </span>
+            <img
+              className="q-referEarn-copy-img"
+              onClick={() => {
+                navigator.clipboard
+                  .writeText(shareCode)
+                  .then(() => setCopy(true));
+              }}
+              src={copy ? tick : copySVG}
+              style={{ cursor: "pointer" }}
+              width="20px"
+              alt="tick"
+            />
+          </div>
+          <div className="q-referShare-content-msg">
+            <p style={style}>Share with your community</p>
+          </div>
+          <div className="q-referShare-content-social">
+            <img
+              className="q-referShare-content-social-img"
+              src={discordSvg}
+              alt="Discord"
+            />
 
-    if (!isOpen) return <></>
-    return (
-        <div className='q-referShare' style={style}>
-            <div className='q-referShare-content'>
-                <img src={closePng} className='q-referShare-content-close' alt="" onClick={() =>onClose && onClose()}/>
-                <div className='q-referShare-content-text'>
-                    <h3 className='q-referShare-content-head' style={style}>Invite your friends</h3>
-                    <p className='q-referShare-content-para' style={style}>
-                        Share your unique referral code with friends and receive 10 coins in credits each time a friend
-                        signs up!
-                    </p>
-                    <div className='q-referShare-content-rect' style={style}>
-                        <span className='q-referShare-content-code' style={style}>{shareCode}</span>
-                        <img src={copy ? tick : copyPng} className='q-referShare-content-copy-img' alt=""
-                             onClick={() => {
-                                 navigator.clipboard.writeText(shareCode).then(() => setCopy(true));
-                             }}/>
-                    </div>
-                    <p className='q-referShare-content-msg' style={style}>Share with your community</p>
-                    <div className='q-referShare-content-social'>
-                        <img onClick={() => shareOnPlatform(shareCode, 'twitter')} src={twitterPng}
-                             className='q-referShare-content-social-img' alt=""/>
-                        <img onClick={() => shareOnPlatform(shareCode, 'telegram')} src={telegramPng}
-                             className='q-referShare-content-social-img' alt=""/>
-                    </div>
-                </div>
-            </div>
+            {/* Twitter */}
+            <img
+              className="q-referShare-content-social-img"
+              onClick={() => shareOnPlatform(shareCode, "twitter")}
+              src={twitterSvg}
+              alt="Twitter"
+            />
+
+            {/* Whatsapp */}
+            <img
+              className="q-referShare-content-social-img"
+              src={whatsappSvg}
+              alt="Whatsapp"
+            />
+            <img
+              className="q-referShare-content-social-img"
+              onClick={() => shareOnPlatform(shareCode, "telegram")}
+              src={telegramPng}
+              alt="telegramPng"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export const ReferEarn = ({
@@ -87,7 +137,7 @@ export const ReferEarn = ({
           <div className="q-referEarn-pop-div">
             <img
               onClick={() => shareOnPlatform(shareCode, "twitter")}
-              src={twitterPng}
+              src={twitterSvg}
               className="q-referShare-content-social-img"
               alt=""
             />
@@ -115,7 +165,7 @@ export const ReferEarn = ({
           {copy ? (
             <img
               className="q-referEarn-copy-img"
-              src={copy ? tick : copyPng}
+              src={tick}
               style={{ cursor: "pointer" }}
               width="20px"
               alt="tick"
