4adc453913e150ac7f8974915240d0b775e02c54
1Gireesh <gireeshbhat68@gmail.com>
2023-11-16 18:18:50 +0530

    updated rerender issue

--- src/components/TutorialScreen/TutorialScreen.tsx
+++ src/components/TutorialScreen/TutorialScreen.tsx
@@ -51,7 +51,6 @@ const Tutorial: React.FC<TutorialProps> = ({
   const [formdata, setFormdata] = useState<TutorialStep[]>([]);
   const [gradient, setGradient] = useState<boolean>(false);
   const [showLoader, setShowLoader] = useState<boolean>(false);
-  const progressBarWidth = `${(completedSteps.length / formdata.length) * 100}%`;
   const [hoverStates, setHoverStates] = useState(
     Array(formdata.length).fill(true)
   );
