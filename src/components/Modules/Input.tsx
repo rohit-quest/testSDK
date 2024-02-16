9a98db88d4b073bd44a81b92cec074a4f3b64b62
Pankaj <pnkjvshsht1@gmail.com>
2024-02-16 15:58:38 +0530

    updated

--- src/components/Modules/Input.tsx
+++ src/components/Modules/Input.tsx
@@ -1,19 +1,36 @@
+import { CSSProperties, ChangeEventHandler } from "react";
 import { emailLogo, phoneLogo, userLogo } from "../../assets/assetsSVG";
+import './css/input.css'
 
+const LogoType = {
+  email: emailLogo,
+  phone: phoneLogo,
+  text: ()=> (<></>),
+  number : phoneLogo
+}
+export type logoType =  keyof typeof LogoType;
+interface InputType {
+  placeholder?: string;
+  inputType?: string;
+  type: keyof typeof LogoType;
+  style?: CSSProperties | undefined;
+  onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
+  iconColor? : string ,
+  value ? : string
+}
 
-const normalInput = (
-    placeholder: string,
-    inputType: string,
-) => {
-    return (
-            <div className="q-onb-input" >
-                <input
-                    type={inputType}
-                    id="normalInput"
-                    name="normalInput"
-                    placeholder={placeholder}
-                    className="q_sdk_input"
-                />
-            </div>
-    );
-};
+export const NormalInput = ({ placeholder, inputType, type, style ,onChange, iconColor,value }: InputType) => {
+  return (
+    <div className="q_input_cont" style={style}>
+      <input
+        type={inputType}
+        name="normalInput"
+        placeholder={placeholder}
+        className="q_input_main_cont"
+        onChange ={onChange}
+        value={value}
+      />
+      {(LogoType[type])(iconColor || '')}
+    </div>
+  );
+}
