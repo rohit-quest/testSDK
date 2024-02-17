48c47c8b1ffe7f4f4814ec66dd024cfdfa85b422
Prabhat Kumar <prabhssgg3@gmail.com>
2024-02-17 17:41:39 +0530

    Login fix

--- src/components/Modules/Input.tsx
+++ src/components/Modules/Input.tsx
@@ -1,25 +1,35 @@
-import { CSSProperties, ChangeEventHandler } from "react";
+import { CSSProperties, ChangeEventHandler, KeyboardEventHandler } from "react";
 import { emailLogo, phoneLogo, userLogo } from "../../assets/assetsSVG";
-import './css/input.css'
+import "./css/input.css";
 
 const LogoType = {
   email: emailLogo,
   phone: phoneLogo,
-  text: ()=> (<></>),
-  number : phoneLogo
-}
-export type logoType =  keyof typeof LogoType;
+  text: () => <></>,
+  number: phoneLogo,
+};
+export type logoType = keyof typeof LogoType;
 interface InputType {
   placeholder?: string;
   inputType?: string;
   type: keyof typeof LogoType;
   style?: CSSProperties | undefined;
   onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
-  iconColor? : string ,
-  value ? : string
+  onKeyUp?: KeyboardEventHandler<HTMLInputElement> | undefined;
+  iconColor?: string;
+  value?: string;
 }
 
-export const NormalInput = ({ placeholder, inputType, type, style ,onChange, iconColor,value }: InputType) => {
+export const NormalInput = ({
+  placeholder,
+  inputType,
+  type,
+  style,
+  onChange,
+  iconColor,
+  value,
+  onKeyUp,
+}: InputType) => {
   return (
     <div className="q_input_cont" style={style}>
       <input
@@ -27,10 +37,11 @@ export const NormalInput = ({ placeholder, inputType, type, style ,onChange, ico
         name="normalInput"
         placeholder={placeholder}
         className="q_input_main_cont"
-        onChange ={onChange}
+        onChange={onChange}
+        onKeyUp={onKeyUp}
         value={value}
       />
-      {(LogoType[type])(iconColor || '')}
+      {LogoType[type](iconColor || "")}
     </div>
   );
-}
+};
