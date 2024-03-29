249c399e85a44891e773cf43cc3139c62768f5c9
Pankaj <pnkjvshsht1@gmail.com>
2024-03-30 02:20:19 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement !== null) {
-  ReactDOM.render(<App />, rootElement);
+if (typeof document !== 'undefined') {
+  const rootElement = document.getElementById("root");
+  if (rootElement !== null) {
+    ReactDOM.render(<App />, rootElement);
+  } else {
+    throw new Error("Root element not found in the document");
+  }
 } else {
-  throw new Error("Root element not found in the document");
+
+  console.error("The document object is not available. Cannot render React app.");
 }
