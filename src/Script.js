a876cab7d10207266c61354b09dc51a1a4dbebc2
rohit-quest <rohit.p@questlabs.biz>
2024-06-13 19:32:25 +0530

    asdfas

--- src/Script.js
+++ src/Script.js
@@ -7,15 +7,32 @@ import ReferEarnPreview from './components/expansion/Preview';
 // ReferEarn
 // ReferEarnPreview
 
+function HelloWorld() {
+    const [count, setCount] = React.useState(0);
+    const classes = document.getElementById('root');
+    console.log(classes)
+    React.useEffect(() => {
+        console.log("use")
+        console.log(count)
+    }, [count]);
+    return (
+        <div>
+            <ReferEarnPreview />
+        </div>
+    );
+}
+// ReactDOM.render(<HelloWorld />, document.getElementById('root'));
 if (typeof document !== 'undefined') {
+    console.log("render")
     const rootElement = document.getElementById('root');
     console.log(rootElement)
     if (rootElement) {
         const root = createRoot(rootElement);
-        root.render(<ReferEarnPreview />);
+        root.render(<HelloWorld />);
     } else {
         console.error('Root element with id "root" not found. Cannot render React app.');
     }
 } else {
+    console.log("Else no render")
     console.error('The document object is not available. Cannot render React app.');
 }
